720998a4414a00d81c1858b364f22bf825c78041
Foram1403 <meet042828mp@gmail.com>
2023-06-26 11:59:35 +0530

    Fifth Commit

---
+++ Dicitonary.py
@@ -0,0 +1,6 @@
+Dict = {'name' : 'Foram' , 'marks' : [25,23,20,25,30]}
+print (Dict)
+print(Dict['marks'][4])
+
+Dict1 = {'name' : 'Foram' , 'age' :{'ages' : [1,2,3,4,5,{'names': 'Ram'}]}}
+print(Dict1['age']['ages'][5]['names'])
